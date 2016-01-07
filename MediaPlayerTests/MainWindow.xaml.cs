5a92ddb8d4a80b8d844391c971455c4ecf31ded6
Mark Wong <mark.sl.wong@gmail.com>
2016-01-06 16:27:34 -0800

    Multithreaded MediaPlayer

--- MediaPlayerTests/MainWindow.xaml.cs
+++ MediaPlayerTests/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Threading;
 using JetBrains.Profiler.Windows.Api;
 
@@ -16,12 +17,14 @@ namespace MediaPlayerTests
     public partial class MainWindow : Window
     {
         private const int MaxVideos = 5;
-        private const int SwitchTimeSeconds = 3;
+        private const int SwitchTimeMilliseconds = 1000;
         private const int DumpEverySwitchCount = int.MaxValue;
 
         private readonly List<StackPanel> _cells = new List<StackPanel>();
-        private readonly MediaElement[] _mediaPlayers;
+        private readonly HostVisual[] _hosts;
+        private readonly MediaPlaybackThread[] _threads;
         private readonly DispatcherTimer _timer;
+        
         private readonly string[] _files;
         private readonly long[] _sizes;
 
@@ -33,7 +36,7 @@ namespace MediaPlayerTests
         public MainWindow()
         {
             InitializeComponent();
-
+            
             _cells.Add(Cell00);
             _cells.Add(Cell01);
             _cells.Add(Cell02);
@@ -59,8 +62,27 @@ namespace MediaPlayerTests
             _cells.Add(Cell42);
             _cells.Add(Cell43);
             _cells.Add(Cell44);
-            
-            _mediaPlayers = new MediaElement[_cells.Count];
+
+            _threads = new MediaPlaybackThread[_cells.Count];
+            _hosts = new HostVisual[_cells.Count];
+
+            for (var i = 0; i < _cells.Count; ++i)
+            {
+                var host = new HostVisual();
+                
+                _hosts[i] = host;
+                _threads[i] = new MediaPlaybackThread(host);
+                
+                var visualWrapper = new VisualWrapper
+                {
+                    Child = host,
+                    Width = 320,
+                    Height = 240,
+                    IsHitTestVisible = false
+                };
+
+                _cells[i].Children.Add(visualWrapper);
+            }
 
             _files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Videos\\IliaSequencer");
             _sizes = new long[_files.Length];
@@ -73,7 +95,7 @@ namespace MediaPlayerTests
             
             _timer = new DispatcherTimer
             {
-                Interval = TimeSpan.FromSeconds(SwitchTimeSeconds)
+                Interval = TimeSpan.FromMilliseconds(SwitchTimeMilliseconds)
             };
 
             _timer.Tick += TimerOnElapsed;
@@ -82,6 +104,8 @@ namespace MediaPlayerTests
                 MemoryProfiler.EnableAllocations();
 
             _timer.Start();
+
+            TimerOnElapsed(this, EventArgs.Empty);
         }
 
         private void TimerOnElapsed(object sender, EventArgs args)
@@ -118,22 +142,9 @@ namespace MediaPlayerTests
 
         private void PlayVideo(int indexCell, int indexFile)
         {
-            Debug.Assert(_mediaPlayers[indexCell] == null);
-
-            var mediaElement = new LeakyMediaElement(_sizes[indexFile]) // The LeakyMediaElement forces an early GC, but even that is not enough
-            {
-                Visibility = Visibility.Visible,
-                LoadedBehavior = MediaState.Manual,
-                UnloadedBehavior = MediaState.Manual
-            };
-
-            mediaElement.MediaEnded += MediaPlayer_Ended;
-
-            _mediaPlayers[indexCell] = mediaElement;
-            _cells[indexCell].Children.Add(mediaElement);
-            
-            mediaElement.Source = new Uri(_files[indexFile]);
-            mediaElement.Play();
+            Debug.Assert(_threads[indexCell] != null);
+            _threads[indexCell].Play(_files[indexFile], 320, 240);
+            Console.WriteLine("Now playing {0} on cell {1}.", _files[indexFile], indexCell);
         }
 
         private void StopVideo(int indexCell, int indexFile)
@@ -141,42 +152,8 @@ namespace MediaPlayerTests
             if (indexCell < 0)
                 return;
 
-            var mediaElement = _mediaPlayers[indexCell];
-
-            Debug.Assert(mediaElement != null);
-
-            mediaElement.UnloadedBehavior = MediaState.Close;
-            mediaElement.MediaEnded -= MediaPlayer_Ended;
-
-            mediaElement.Stop();
-            mediaElement.Source = null;
-
-            _mediaPlayers[indexCell] = null;
-            _cells[indexCell].Children.Clear();
-        }
-
-        private static void MediaPlayer_Ended(object sender, RoutedEventArgs args)
-        {
-            var mediaElement = sender as MediaElement;
-            Debug.Assert(mediaElement != null);
-            mediaElement.Position = TimeSpan.Zero; // Repeats the video
-        }
-
-        private class LeakyMediaElement : MediaElement
-        {
-            private readonly long _memoryPressure;
-
-            public LeakyMediaElement(long memoryPressure)
-            {
-                _memoryPressure = memoryPressure;
-
-                GC.AddMemoryPressure(_memoryPressure);
-            }
-
-            ~LeakyMediaElement()
-            {
-                GC.RemoveMemoryPressure(_memoryPressure);
-            }
+            Debug.Assert(_threads[indexCell] != null);
+            _threads[indexCell].Stop();
         }
     }
 }
---
+++ MediaPlayerTests/MediaPlaybackThread.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+
+namespace MediaPlayerTests
+{
+    public class MediaPlaybackThread : IDisposable
+    {
+        private readonly HostVisual _HostVisual;
+        private readonly Thread _Thread;
+
+        private VisualTarget _VisualTarget;
+        private LeakyMediaElement _MediaElement;
+
+        private VisualTargetPresentationSource _VisualTargetSource;
+
+        private volatile bool _CanPlay;
+        private PlaybackInfo _NextToPlay;
+
+        private class PlaybackInfo
+        {
+            public string FileName { get; set; }
+            public double Width { get; set; }
+            public double Height { get; set; }
+        }
+
+        public MediaPlaybackThread(HostVisual hostVisual)
+        {
+            _HostVisual = hostVisual;
+            _CanPlay = true;
+            
+            _Thread = new Thread(Run)
+            {
+                IsBackground = true
+            };
+
+            _Thread.SetApartmentState(ApartmentState.STA);
+            _Thread.Start();
+        }
+
+        public void Dispose()
+        {
+            lock (_Thread)
+            {
+                Dispatcher dispatcher = Dispatcher.FromThread(_Thread);
+                if (dispatcher != null)
+                    dispatcher.BeginInvokeShutdown(DispatcherPriority.Normal);
+                else
+                    _Thread.Abort();
+            }
+        }
+
+        private void MediaPlayer_Opened(object sender, RoutedEventArgs args)
+        {
+            _CanPlay = true;
+
+            lock (_Thread)
+            {
+                if (_NextToPlay != null)
+                {
+                    DestroyMediaPlayer();
+                    CreateMediaPlayer();
+                }
+            }
+        }
+
+        private void MediaPlayer_Ended(object sender, RoutedEventArgs args)
+        {
+            lock (_Thread)
+            {
+                MediaElement mediaElement = sender as MediaElement;
+                Debug.Assert(mediaElement != null);
+                mediaElement.Position = TimeSpan.Zero; // Repeats the video
+            }
+        }
+
+        private void MediaPlayer_Failed(object sender, RoutedEventArgs arg)
+        {
+            lock (_Thread)
+            {
+                DestroyMediaPlayer();
+
+                if (_CanPlay && _NextToPlay != null)
+                {
+                    CreateMediaPlayer();
+                }
+            }
+        }
+        
+        public void Play(string fileName, double width, double height)
+        {
+            Dispatcher dispatcher;
+
+            lock (_Thread)
+            {
+                _NextToPlay = new PlaybackInfo
+                {
+                    FileName = fileName,
+                    Width = width,
+                    Height = height
+                };
+
+                dispatcher = Dispatcher.FromThread(_Thread);
+
+                if (dispatcher == null)
+                    return;
+            }
+
+            dispatcher.InvokeAsync(() =>
+            {
+                lock (_Thread)
+                {
+                    if (_CanPlay)
+                    {
+                        DestroyMediaPlayer();
+                        CreateMediaPlayer();
+                    }
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            Dispatcher dispatcher = Dispatcher.FromThread(_Thread);
+
+            if (dispatcher != null)
+            {
+                dispatcher.InvokeAsync(() =>
+                {
+                    lock (_Thread)
+                    {
+                        DestroyMediaPlayer();
+                    }
+                });
+            }
+        }
+
+        private void CreateMediaPlayer()
+        {
+            _MediaElement = new LeakyMediaElement
+            {
+                LoadedBehavior = MediaState.Manual,
+                UnloadedBehavior = MediaState.Manual,
+                IsMuted = true,
+                IsHitTestVisible = false
+            };
+
+            _VisualTargetSource.RootVisual = _MediaElement;
+
+            _MediaElement.MediaOpened += MediaPlayer_Opened;
+            _MediaElement.MediaEnded += MediaPlayer_Ended;
+            _MediaElement.MediaFailed += MediaPlayer_Failed;
+
+            if (_CanPlay && _NextToPlay != null)
+            {
+                _CanPlay = false;
+
+                FileInfo fileInfo = new FileInfo(_NextToPlay.FileName);
+                _MediaElement.SetMemoryPressure(fileInfo.Length);
+
+                _MediaElement.Source = new Uri(_NextToPlay.FileName);
+                _MediaElement.Width = _NextToPlay.Width;
+                _MediaElement.Height = _NextToPlay.Height;
+                _MediaElement.Play();
+
+                _NextToPlay = null;
+            }
+        }
+
+        private void DestroyMediaPlayer()
+        {
+            if (_MediaElement != null)
+            {
+                _MediaElement.MediaOpened -= MediaPlayer_Opened;
+                _MediaElement.MediaEnded -= MediaPlayer_Ended;
+                _MediaElement.MediaFailed -= MediaPlayer_Failed;
+                
+                _MediaElement.Close();
+
+                _MediaElement = null;
+            }
+
+            _CanPlay = true;
+            
+            _VisualTargetSource.RootVisual = null;
+        }
+
+        private void Run()
+        {
+            try
+            {
+                _VisualTarget = new VisualTarget(_HostVisual);
+                _VisualTargetSource = new VisualTargetPresentationSource(_VisualTarget);
+                
+                lock (_Thread)
+                {
+                    CreateMediaPlayer();
+                }
+
+                _VisualTargetSource.RootVisual = _MediaElement;
+
+                Dispatcher.Run();
+            }
+            finally
+            {
+                if (_VisualTarget != null)
+                {
+                    _VisualTarget.Dispose();
+
+                    GC.SuppressFinalize(_VisualTarget);
+                }
+
+                lock (_Thread)
+                {
+                    DestroyMediaPlayer();
+
+                    GC.SuppressFinalize(_MediaElement);
+                }
+
+                _CanPlay = false;
+            }
+        }
+
+        private class VisualTargetPresentationSource : PresentationSource
+        {
+            private readonly VisualTarget _VisualTarget;
+
+            public VisualTargetPresentationSource(VisualTarget visualTarget)
+            {
+                _VisualTarget = visualTarget;
+            }
+
+            protected override CompositionTarget GetCompositionTargetCore()
+            {
+                return _VisualTarget;
+            }
+
+            public override Visual RootVisual
+            {
+                get { return _VisualTarget.RootVisual; }
+                set
+                {
+                    Visual oldRoot = _VisualTarget.RootVisual;
+
+                    _VisualTarget.RootVisual = value;
+
+                    RootChanged(oldRoot, value);
+
+                    UIElement rootElement = value as UIElement;
+                    if (rootElement != null)
+                    {
+                        rootElement.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                        rootElement.Arrange(new Rect(rootElement.DesiredSize));
+                    }
+                }
+            }
+
+            public override bool IsDisposed
+            {
+                get { return false; }
+            }
+        }
+
+        private class LeakyMediaElement : MediaElement
+        {
+            private long _MemoryPressure;
+            
+            public void SetMemoryPressure(long bytesAllocated)
+            {
+                if (_MemoryPressure > 0)
+                    GC.RemoveMemoryPressure(_MemoryPressure);
+
+                _MemoryPressure = bytesAllocated;
+                GC.AddMemoryPressure(_MemoryPressure);
+            }
+
+            ~LeakyMediaElement()
+            {
+                if (_MemoryPressure > 0)
+                    GC.RemoveMemoryPressure(_MemoryPressure);
+            }
+        }
+    }
+}
